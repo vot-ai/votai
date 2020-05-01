908d89cd846e5fe626fa69a51ced9441237fb801
Maronato <gustavomaronato@gmail.com>
2020-05-01 18:15:52 -0300

    Added epsilon constant

--- backend/crowd_bt/constants.py
+++ backend/crowd_bt/constants.py
@@ -15,3 +15,34 @@ Used by equations (9) and (10)
 Represents the tradeoff between exploration and exploitation
 """
 GAMMA: Final[float] = 5.0
+
+"""Epsilon (ε)
+
+ε-greedy variable to select random item from pool instead
+of the one that maximizes information gain
+"""
+EPSILON: Final[float] = 0.25
+
+"""Initial Mu (μ)
+
+Initial value for object's μ
+"""
+MU: Final[float] = 0.0
+
+"""Initial Sigma Squared (σ²)
+
+Initial value for object's σ²
+"""
+SIGMA_SQUARED: Final[float] = 1.0
+
+"""Initial Alpha (ɑ)
+
+Initial ɑ. Assumes annotator is good.
+"""
+ALPHA: Final[float] = 10
+
+"""Initial Beta (β)
+
+Initial β. Assumes annotator is good.
+"""
+BETA: Final[float] = 1
