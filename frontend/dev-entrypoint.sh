f6f3f83d6b4d47830d4749fce07d2479d8523995
Maronato <gustavomaronato@gmail.com>
2020-05-04 18:38:50 -0300

    added frontend

---
+++ frontend/dev-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Check that mongo is up and running on port `27017`:
+# dockerize -wait 'tcp://mongo:27017' -timeout 30s -wait-retry-interval 3s
+
+# Install dependencies on local machine
+echo "Installing dev dependencies from yarn"
+yarn
+echo "Done! Executing command"
+exec "$@"
---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Check that mongo is up and running on port `27017`:
+dockerize -wait 'tcp://mongo:27017' -timeout 30s -wait-retry-interval 3s
+
+echo "Done! Executing command"
+exec "$@"
