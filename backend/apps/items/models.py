8e530dd210d9dc6371742fef3c94b0a6a9cacb93
Maronato <gustavomaronato@gmail.com>
2020-05-09 15:44:49 -0300

    auto revoke deactivation tasks

--- backend/apps/items/models.py
+++ backend/apps/items/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 from backend.fields import ShortUUIDField
+from backend.celery import app as celery_app
 from apps.surveys.models import Survey
 from apps.crowd_bt.constants import MU, SIGMA_SQUARED
 from apps.crowd_bt.types import Mu, SigmaSquared, RelevanceScore
@@ -24,6 +25,11 @@ class Item(models.Model):
     def score(self) -> RelevanceScore:
         return RelevanceScore(self.mu, self.sigma_squared)
 
+    @property
+    def auto_deactivate_task_id(self) -> str:
+        current_annotator_uuid = self.current_annotators.only("uuid").first().uuid
+        return f"auto_deactivate_{self.uuid}_{current_annotator_uuid}"
+
     def prioritize(self) -> None:
         self.prioritized = True
         self.save()
@@ -39,6 +45,8 @@ class Item(models.Model):
     def deactivate(self) -> None:
         self.active = False
         self.save()
+        # Cancel auto deactivation tasks
+        celery_app.control.revoke(self.auto_deactivate_task_id)
 
     def update_score(self, new_score: RelevanceScore) -> None:
         self.mu = new_score.mu  # pylint: disable=invalid-name
@@ -47,4 +55,6 @@ class Item(models.Model):
 
     def schedule_deactivate(self, max_time: int) -> None:
         if max_time > 0:
-            auto_deactivate.apply_async((self.pk,), countdown=max_time)
+            auto_deactivate.apply_async(
+                (self.pk,), countdown=max_time, task_id=self.auto_deactivate_task_id
+            )
