7cc6736698c2ed0617fe4607b40d6da6ea5238c8
Maronato <gustavomaronato@gmail.com>
2020-05-12 19:10:16 -0300

    remove unused

--- frontend/layouts/firstPage.vue
+++ frontend/layouts/firstPage.vue
@@ -8,40 +8,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'nuxt-composition-api'
+import { defineComponent } from 'nuxt-composition-api'
 import BaseLayout from './baseLayout.vue'
 import AppBar from '~/components/layouts/AppBar.vue'
 
 export default defineComponent({
   layout: 'empty',
-  components: { BaseLayout, AppBar },
-  setup() {
-    const scrolled = ref(false)
-    const onScroll = () => {
-      if (window.scrollY > 0) {
-        scrolled.value = true
-      } else {
-        scrolled.value = false
-      }
-    }
-
-    const transparentColor = computed(() =>
-      !scrolled.value ? 'transparent' : undefined
-    )
-    return {
-      onScroll,
-      transparentColor
-    }
-  }
+  components: { BaseLayout, AppBar }
 })
 </script>
 
-<style scoped>
-.logo {
-  font-family: 'Major Mono Display', monospace;
-  font-size: 3rem;
-}
-</style>
 <style>
 .v-content {
   padding-top: 0px !important;
