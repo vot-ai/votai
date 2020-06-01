d4447986c7fa3536df7ebc329a55c10a08d6f7df
Maronato <gustavomaronato@gmail.com>
2020-06-01 17:03:26 -0300

    fix prod dockerfiles and entrypoints

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,5 +3,7 @@
 # Check that mongo is up and running on port `27017`:
 dockerize -wait 'tcp://mongo:27017' -timeout 30s -wait-retry-interval 3s
 
+echo "Building locally"
+yarn build
 echo "Done! Executing command"
 exec "$@"
